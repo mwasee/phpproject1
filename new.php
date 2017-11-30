c27eb2bb0031e8aa6afb1f07b2fefd6f62a4f963
mwasee68 <mwasee68@gmail.com>
2017-11-30 13:10:39 +0530

    hello

---
+++ new.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="css/phpdemo.css" rel="stylesheet" type="text/css"/>
+        <title>mohd</title>
+       
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        <input type="submit" value="ok" /> welcome    <textarea name="" rows="4" cols="20">my page's
+        </textarea>  
+        <h1>this is a new page</h1>
+        <div>
+        <div class="row">
+            <div class="col-md-6 menu">
+                gnbkghjnfjng
+            </div>
+            <div class="col-md-6 menu">
+                nfgdhrtlhndiguhfff
+            </div>
+        </div>
+        </div>
+    
+        <h2><select name="my">
+                <option>ABC</option>
+                <option>XYZ</option>
+            </select>
+     <form>
+            <table>
+                <tr>
+                    <th><lable>Name</lable></th>
+                    <td><input type="text"  name="name"/></td>
+                </tr>
+                <tr>
+                    <th><lable>Class</lable></th>
+   <td><select name="class" style="width: 100px;">
+  <option>first</option>
+<option >second</option>
+       <option >third</option>
+       <option >fourth</option>
+          </select>
+                    </td>                 
+          </tr>
+                <tr>
+                    <th><lable>Age</lable></th>
+                <td><input type="number"/></td>
+                </tr>
+                <th><lable>Contact </lable></th>
+                 <td><input type="number"/></td>
+            <tr>
+            <th><label>Address</label></th>
+                <td><input type="text" /></td>
+            </tr>
+          <tr>
+              <th><lable>E-mail</lable></th>
+              <td><input type="text" /></td>
+          </tr>
+                           <tr>
+                    <th><lable>Father's name</lable></th>
+                    <td><input type="text"/></td>
+            </tr>
+            <tr>
+                <th><lable>Gender</lable></th>
+            <td>male<input type="radio" value="male" name="a">&nbsp;&nbsp;&nbsp;&nbsp; female<input type="radio" value="female" name="a"></td>
+            </tr>
+ <tr>
+            <td></td>
+            <td><input type="submit" value="Save" />&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" value="Reset"/></td>
+            </tr>
+            <tr>
+                <th><lable>Qualification</lable></th>
+            <td><input type="checkbox" value=/></td>
+            
+</table>
+</form>
+<script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="js/jquery-3.2.1.slim.min.js" type="text/javascript"></script>
+    <script src="js/popper.min.js" type="text/javascript"></script>
+    </body>
+</html>
